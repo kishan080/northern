f6bb15e9b656eddb46bf801ff6fe32b0744d2a18
kishan080 <kishan080@outlook.com>
2022-11-15 18:59:39 +0530

    final

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,19 +10,74 @@ const options = [
   {
     label: "USD",value: "USD"
   },
-  { label: "EUR",value: "EUR" },
-  { label: "GBP",value: "GBP" },
-  { label: "JPY",value: "JPY" },
-  { label: "AUD",value: "AUD" },
-  { label: "CAD",value: "CAD" },
-  { label: "CHF",value: "CHF" },
-  { label: "CNY",value: "CNY" },
-  { label: "INR",value: "INR" },
+  
+  {
+    label: "INR",value: "INR"
+  },
+  
+  {
+    label: "DZD",value: "DZD"
+  },
+  
+  {
+    label: "AUD",value: "AUD"
+  },
+  
+  {
+    label: "BHD",value: "BHD"
+  },
+  {
+    label: "VEF",value: "VEF"
+  },
+  {
+    label: "BWP",value: "BWP"
+  },
+  {
+    label: "BRL",value: "BRL"
+  },
+  {
+    label: "BND",value: "BND"
+  },
+  {
+    label: "CAD",value: "CAD"
+  },
+  {
+    label: "CNP",value: "CNP"
+  },
+  {
+    label: "COP",value: "COP"
+  },
+  {
+    label: "CZK",value: "CZK"
+  },
+  {
+    label: "DKK",value: "DKK"
+  },
+  {
+    label: "HUF",value: "HUF"
+  },
+  {
+    label: "IDR",value: "IDR"
+  },
+  {
+    label: "IRR",value: "IRR"
+  },
+  {
+    label: "JPY",value: "JPY"
+  },
+  {
+    label: "KZT",value: "KZT"
+  },
+  {
+    label: "KRW",value: "KRW"
+  },
+  
 ]
 
  
 
 const yearOpt = [
+{ label: "2022",value: "2022" },
 { label: "2021",value: "2021" },
 { label: "2020",value: "2020" },
 { label: "2019",value: "2019" },
